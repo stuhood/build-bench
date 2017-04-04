e7440705ff22cc54947eb34caab39fa058420076
Thibault Kruse <thibault.kruse@gmx.de>
2017-04-04 18:36:39 +0900

    one less step necessary to reproduce

--- benchmarks/commons-math/commons-math.sh
+++ benchmarks/commons-math/commons-math.sh
@@ -7,8 +7,6 @@ rm -rf ../../caches/bazel/bazel0.3.1/bazel_cache
 make -C ../.. CONFIG=configs/commons-math.mk
 mv ../../build/reports/commons-math.csv ./sample1.csv
 
-make -C ../.. CONFIG=configs/commons-math.mk
-mv ../../build/reports/commons-math.csv ./sample2.csv
 
 cp ../../build/commons-math/source/src/main/java/org/apache/commons/math3/linear/AbstractFieldMatrix.java .
 cp AbstractFieldMatrix.java.modified ../../build/commons-math/source/src/main/java/org/apache/commons/math3/linear/AbstractFieldMatrix.java
@@ -19,9 +17,3 @@ make -C ../.. CONFIG=configs/commons-math.mk
 mv ../../build/reports/commons-math.csv ./sample3.csv
 
 
-cp -f AbstractFieldMatrix.java ../../build/commons-math/source/src/main/java/org/apache/commons/math3/linear/AbstractFieldMatrix.java
-# ensure Make spreads the change to the buildsystem folders
-touch ../../build/commons-math/source
-
-make -C ../.. CONFIG=configs/commons-math.mk
-mv ../../build/reports/commons-math.csv ./sample4.csv
